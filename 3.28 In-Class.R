17bd212292fda7a1581ad64083ba126cce555a56
JohnnyLi99 <98190340+JohnnyLi99@users.noreply.github.com>
2022-03-28 11:33:26 -0400

    Add files via upload

---
+++ 3.28 In-Class.R
@@ -0,0 +1,57 @@
+#March 28 IN CLASS Exercise
+
+#import dataset
+data("iris")
+head(iris) # inspecting the top rows of the dataset
+str(iris) # structure of the dataset
+#import the library
+library(ggplot2)
+library(e1071)
+
+qplot(Petal.Length, Petal.Width, data=iris, color = Species)
+
+### MODEL 1
+svm_model1 <- svm(Species~., data = iris)
+#summary
+summary(svm_model1)
+#plot
+plot(svm_model1, data = iris,
+     Petal.Width~Petal.Length, slice =
+       list(Sepal.Width = 3, Sepal.Length = 4))
+pred1 <- predict(svm_model1, iris)
+table1 <- table(Predicted = pred1, Actual = iris$Species)
+table1
+Model1_accuracyRate = sum(diag(table1))/sum(table1)
+Model1_accuracyRate
+Model1_MissClassificationRate = 1 - Model1_accuracyRate
+Model1_MissClassificationRate
+
+### MODEL 2
+svm_model2 <- svm(Species~., data = iris, kernel = "linear")
+#summary
+summary(svm_model2)
+plot(svm_model2, data = iris,
+     Petal.Width~Petal.Length, slice = list(Sepal.Width = 3,
+                                            Sepal.Length = 4))
+pred2 <- predict(svm_model2, iris)
+table2 <- table(Predicted = pred2, Actual = iris$Species)
+table2
+Model2_accuracyRate = sum(diag(table2))/sum(table2)
+Model2_accuracyRate
+Model2_MissClassificationRate = 1 - Model2_accuracyRate
+Model2_MissClassificationRate
+
+### MODEL 3
+svm_model3 <- svm(Species~., data = iris, kernel = "polynomial")
+#summary
+summary(svm_model3)
+plot(svm_model3, data = iris,
+     Petal.Width~Petal.Length, slice = list(Sepal.Width = 3,
+                                            Sepal.Length = 4))
+pred3 <- predict(svm_model3, iris)
+table3 <- table(Predicted = pred3, Actual = iris$Species)
+table3
+Model3_accuracyRate = sum(diag(table3))/sum(table3)
+Model3_accuracyRate
+Model3_MissClassificationRate = 1 - Model3_accuracyRate
+Model3_MissClassificationRate
